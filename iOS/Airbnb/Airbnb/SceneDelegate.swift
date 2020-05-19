4a266458254b362dc1e8a1857b224c4ee19c164d
ehgud0670 <kimdo2297@gmail.com>
2020-05-19 22:29:39 +0900

    [#11] chore: 줄간격 정리

--- iOS/Airbnb/Airbnb/SceneDelegate.swift
+++ iOS/Airbnb/Airbnb/SceneDelegate.swift
@@ -12,10 +12,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let scene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: scene)
         
+        window = UIWindow(windowScene: scene)
         window?.rootViewController = tabBarController()
     }
     
