33129247b30f174b5f126e36395055ba379c94d6
seizze <cwcwc3@gmail.com>
2020-05-19 14:16:39 +0900

    [pair][#3] chore: 폴더 구조 생성

---
+++ iOS/Airbnb/Airbnb/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/05/19.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
