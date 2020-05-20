b5fb39928bf143f79f05e9840c9c1fd33fc3d21d
ehgud0670 <kimdo2297@gmail.com>
2020-05-20 21:39:34 +0900

    [#6] feat: 뱃지 레이블 생성 	* frame, coder 생성자 붙임 	* final 로 선언함 ( 상속 x )

---
+++ iOS/Airbnb/Airbnb/Views/BadgeLabel.swift
@@ -0,0 +1,19 @@
+//
+//  BadgeLabel.swift
+//  Airbnb
+//
+//  Created by kimdo2297 on 2020/05/20.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+final class BadgeLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
