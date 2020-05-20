77283dacc4bb30389dfa8598161780d458cf6d59
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-20 15:18:44 +0900

    chore: project initalization

---
+++ BE/src/main/java/dev/codesquad/airbnb02/Airbnb02Application.java
@@ -0,0 +1,13 @@
+package dev.codesquad.airbnb02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airbnb02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Airbnb02Application.class, args);
+	}
+
+}
---
+++ BE/src/test/java/dev/codesquad/airbnb02/Airbnb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codesquad.airbnb02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Airbnb02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
