8c6dadeac196880e266e96e571b48f5147925328
ehgud0670 <kimdo2297@gmail.com>
2020-05-21 19:38:24 +0900

    [#5] feat: image를 paging하는 scrollview 구현

---
+++ iOS/Airbnb/Airbnb/Views/ImagePagingView.swift
@@ -0,0 +1,19 @@
+//
+//  ImagePagingView.swift
+//  Airbnb
+//
+//  Created by kimdo2297 on 2020/05/21.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+final class ImagePagingView: UIScrollView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
