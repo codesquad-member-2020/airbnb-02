3b800f0e982650202cb1d17e930ec6e5abb5397b
ehgud0670 <kimdo2297@gmail.com>
2020-05-21 14:32:45 +0900

    [#8] feat: 검색화면에서 사용자에게 안내문구를 보여주는 GuideLabel 구현 	 * 상속이 안될 것이기 때문에 final를 명시함 	 * frame, coder  init 모두 코드 작성함

---
+++ iOS/Airbnb/Airbnb/Views/GuideLabel.swift
@@ -0,0 +1,19 @@
+//
+//  GuideLabel.swift
+//  Airbnb
+//
+//  Created by kimdo2297 on 2020/05/21.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+final class GuideLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
