67d2482f081501beec809b0ca82204c18ca20e84
ehgud0670 <kimdo2297@gmail.com>
2020-05-21 13:20:21 +0900

    [#7] refactor: convexLayer를 삭제하고, 기존에 Layer로 기존의 설정값을 설정함

--- iOS/Airbnb/Airbnb/Views/SearchBar.swift
+++ iOS/Airbnb/Airbnb/Views/SearchBar.swift
@@ -10,21 +10,6 @@ import UIKit
 
 @IBDesignable
 final class SearchBar: UITextField {
-    @IBInspectable var layerColor: UIColor = .clear {
-        didSet { configureLayer() }
-    }
-    
-    private let convexLayer: CALayer = {
-        let layer = CALayer()
-        layer.cornerRadius = 5
-        layer.backgroundColor = UIColor.white.cgColor
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 0, height: 0)
-        layer.shadowRadius = 6
-        layer.shadowOpacity = 0.2
-        return layer
-    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureLayer()
@@ -36,8 +21,10 @@ final class SearchBar: UITextField {
     }
     
     private func configureLayer() {
-        convexLayer.backgroundColor = layerColor.cgColor
-        convexLayer.frame = CGRect(origin: bounds.origin, size: bounds.size)
-        layer.addSublayer(convexLayer)
+        layer.cornerRadius = 5
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowRadius = 6
+        layer.shadowOpacity = 0.2
     }
 }
