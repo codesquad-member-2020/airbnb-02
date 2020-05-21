05970774d54095019ea94bc1b62ee5e3b473f8a3
seizze <cwcwc3@gmail.com>
2020-05-21 16:00:51 +0900

    [#14] feat: 3가지 필터 버튼을 갖는 Filter View 구현

---
+++ iOS/Airbnb/Airbnb/Views/FilterView.swift
@@ -0,0 +1,28 @@
+//
+//  FilterView.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/05/21.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class FilterView: UIStackView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        axis = .horizontal
+        spacing = 8
+    }
+}
