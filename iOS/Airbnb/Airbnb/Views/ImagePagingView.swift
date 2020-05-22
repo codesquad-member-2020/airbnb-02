2d5d695bacf39dff5d872af7f74eea2fa8dc6fbd
ehgud0670 <kimdo2297@gmail.com>
2020-05-22 19:27:36 +0900

    [#5] feat: ImagePagingView를 scrollview의 델리게이트로 설정하고, 	   스크롤할 때 contentOffset의 x값을 frame max X 값으로 나누어서 나머지가 0이 될때            currentPage의 값을 contentOffset / maxX 로 정해서            currentPage가 바뀌도록 기능 구현함

--- iOS/Airbnb/Airbnb/Views/ImagePagingView.swift
+++ iOS/Airbnb/Airbnb/Views/ImagePagingView.swift
@@ -18,12 +18,48 @@ final class ImagePagingView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         insertXibView()
+        configureScrollViewDelegate()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         insertXibView()
+        configureScrollViewDelegate()
+    }
+    
+    private func configureScrollViewDelegate() {
+        scrollView.delegate = self
     }
 }
 
 extension ImagePagingView: XibRepresentable { }
+
+extension ImagePagingView: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let dividend = scrollView.contentOffset.x
+        let divisor = scrollView.frame.maxX
+        guard let remainder = modular(
+            dividend: dividend,
+            divisor: divisor
+            ), remainder == 0 else { return }
+        
+        guard let currentPage = quotient(
+            dividend: dividend,
+            divisor: divisor
+            ) else { return }
+        
+        pageControl.currentPage = currentPage
+    }
+    
+    private func modular(dividend: CGFloat, divisor: CGFloat) -> CGFloat? {
+        guard divisor != 0 else { return nil }
+        return dividend.truncatingRemainder(dividingBy: divisor)
+    }
+    
+    private func quotient(dividend: CGFloat, divisor: CGFloat) -> Int? {
+        guard divisor != 0 else { return nil }
+        let quotient = dividend / divisor
+        return Int(quotient)
+    }
+}
+
