eac21af55dd29b333613b58cf98c445f83cf7d9d
ehgud0670 <kimdo2297@gmail.com>
2020-05-22 11:42:56 +0900

    [#27] refactor: GuideLabelViewModel 삭제 및 GuideLabel 렌더링 로직 리팩토링 	이유는 저번 커밋과 동일합니다.

--- iOS/Airbnb/Airbnb/Views/GuideLabel.swift
+++ iOS/Airbnb/Airbnb/Views/GuideLabel.swift
@@ -22,11 +22,11 @@ final class GuideLabel: UILabel {
     }
     
     private func configureText() {
-        text = GuideLabelViewModel.Text.default
-        textColor = GuideLabelViewModel.Color.defaultText
+        text = "날짜와 인원을 선택하시면 가격별 숙소를 추천해드립니다."
+        textColor = UIColor.dimGray
     }
     
     private func configureFont() {
-        font = GuideLabelViewModel.Font.default
+        font = UIFont.systemFont(ofSize: 15.5, weight: .medium)
     }
 }
