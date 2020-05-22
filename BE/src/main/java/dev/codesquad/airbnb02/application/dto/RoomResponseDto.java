ed6408fde0c891d09cd6e1ece2c11535254e8298
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-22 15:09:54 +0900

    [#24] feat: RoomService to return all rooms as RoomResponseDto

--- BE/src/main/java/dev/codesquad/airbnb02/application/dto/RoomResponseDto.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/dto/RoomResponseDto.java
@@ -1,36 +1,48 @@
 package dev.codesquad.airbnb02.application.dto;
 
+import dev.codesquad.airbnb02.domain.room.domain.Image;
+import dev.codesquad.airbnb02.domain.room.domain.Room;
+import java.util.List;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
 public class RoomResponseDto {
-    private Long id;
-    private String title;
-    private String type;
-    private String location;
-    private String imageUrl;
-    private Integer price;
-    private boolean favorite;
-    private ReviewDto review;
-    private boolean superhost;
 
-    public RoomResponseDto() {}
+  private Long id;
+  private String title;
+  private String type;
+  private String location;
+  private List<Image> images;
+  private Integer price;
+  private boolean favorite;
+  private ReviewDto review;
+  private boolean superhost;
 
-    private RoomResponseDto(Long id, String title, String type, String location, String imageUrl, Integer price, boolean favorite, ReviewDto review, boolean superhost) {
-        this.id = id;
-        this.title = title;
-        this.type = type;
-        this.location = location;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.favorite = favorite;
-        this.review = review;
-        this.superhost = superhost;
-    }
+  public RoomResponseDto() {
+  }
 
-    public static RoomResponseDto create(Long id, String title, String type, String location, String imageUrl, Integer price, boolean favorite, ReviewDto review, boolean superhost) {
-        return new RoomResponseDto(id, title, type, location, imageUrl, price, favorite, review, superhost);
-    }
+  private RoomResponseDto(Long id, String title, String type, String location, List<Image> images,
+      Integer price, boolean favorite, ReviewDto review, boolean superhost) {
+    this.id = id;
+    this.title = title;
+    this.type = type;
+    this.location = location;
+    this.images = images;
+    this.price = price;
+    this.favorite = favorite;
+    this.review = review;
+    this.superhost = superhost;
+  }
+
+//    public static RoomResponseDto create(Long id, String title, String type, String location, String imageUrl, Integer price, boolean favorite, ReviewDto review, boolean superhost) {
+//        return new RoomResponseDto(id, title, type, location, imageUrl, price, favorite, review, superhost);
+//    }
+
+  public static RoomResponseDto create(Room room) {
+    return new RoomResponseDto(room.getId(), room.getName(), "아파트", room.getLocation(),
+        room.getImages(), room.getPrice(), false,
+        ReviewDto.create(room.getReviewRating(), room.getReviewCount()), false);
+  }
 }
--- BE/src/main/java/dev/codesquad/airbnb02/domain/room/business/RoomService.java
+++ BE/src/main/java/dev/codesquad/airbnb02/domain/room/business/RoomService.java
@@ -1,5 +1,27 @@
 package dev.codesquad.airbnb02.domain.room.business;
 
+import dev.codesquad.airbnb02.application.dto.RoomResponseDto;
+import dev.codesquad.airbnb02.domain.room.data.RoomRepository;
+import dev.codesquad.airbnb02.domain.room.domain.Room;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class RoomService {
 
+  private final RoomRepository roomRepository;
+
+  public RoomService(RoomRepository roomRepository) {
+    this.roomRepository = roomRepository;
+  }
+
+  @Transactional(readOnly = true)
+  public List<RoomResponseDto> findAll() {
+
+    return roomRepository.findAll().stream()
+        .map(RoomResponseDto::create)
+        .collect(Collectors.toList());
+  }
 }
