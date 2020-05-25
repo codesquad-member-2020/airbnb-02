7c4762dd55ee06a4f2bc56f0f9ac78925dc4c17d
seizze <cwcwc3@gmail.com>
2020-05-26 00:22:53 +0900

    [#49] feat: 필터 푸터 뷰 커스텀 클래스 구현

---
+++ iOS/Airbnb/Airbnb/Views/FilterFooterView.swift
@@ -0,0 +1,31 @@
+//
+//  FilterFooterView.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/05/26.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class FilterFooterView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+
+    private func configureView() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
+
+extension FilterFooterView: NibLoadable { }
