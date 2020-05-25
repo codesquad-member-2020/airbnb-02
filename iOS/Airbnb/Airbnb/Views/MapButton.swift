b4035092b760e79c1a0c9417abe0eebb06e89fca
ehgud0670 <kimdo2297@gmail.com>
2020-05-25 17:22:56 +0900

    [#40] refactor: 액션 관련 메소드 및 설정 삭제 	  이유: 해당 버튼을 가지는 뷰컨에서 액션을 IBAction으로 설정할 예정이다.

--- iOS/Airbnb/Airbnb/Views/MapButton.swift
+++ iOS/Airbnb/Airbnb/Views/MapButton.swift
@@ -17,36 +17,20 @@ final class MapButton: UIButton {
     @IBInspectable var diameter: CGFloat = 50 {
         didSet { configureDiameter() }
     }
-    
-    var action: (() -> Void)?
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureShadow(offset: CGSize(width: 5, height: 5), opacity: 3)
-        configureAction()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureShadow(offset: CGSize(width: 5, height: 5), opacity: 3)
-        configureAction()
-    }
-    
-    deinit {
-        removeTarget(self, action: #selector(invokeAction), for: .touchUpInside)
     }
     
     private func configureDiameter() {
         widthAnchor.constraint(equalToConstant: diameter).isActive = true
         heightAnchor.constraint(equalToConstant: diameter).isActive = true
-    }
-    
-    private func configureAction() {
-        addTarget(self, action: #selector(invokeAction), for: .touchUpInside)
-    }
-    
-    @objc private func invokeAction(sender: MapButton) {
-        action?()
     }
 }
 
