a23cf4233d16905e6ee8bae908b50ae43d1e8010
seizze <cwcwc3@gmail.com>
2020-05-26 01:02:31 +0900

    [#49] feat: Filter View Controller 구현

---
+++ iOS/Airbnb/Airbnb/Controllers/FilterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FilterViewController.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/05/26.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.4)
+    }
+}
