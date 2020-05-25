c0e622c34451af344a22dfbc6ec5fbaa920a7dda
wooody92 <pkower4@naver.com>
2020-05-25 18:37:29 +0900

    [#52] feat : 전체 숙소 리스트와 필터링 된 숙소 리스트를 가져오는 메서드 분리
    - RoomController.java : 숙소 리스트를 가져오는 메서드 분리했습니다.

--- BE/src/main/java/dev/codesquad/airbnb02/application/controller/RoomController.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/controller/RoomController.java
@@ -1,44 +1,41 @@
 package dev.codesquad.airbnb02.application.controller;
 
-import dev.codesquad.airbnb02.domain.host.data.HostRepository;
+import dev.codesquad.airbnb02.application.dto.RoomResponseDto;
 import dev.codesquad.airbnb02.domain.room.business.RoomService;
-import dev.codesquad.airbnb02.domain.room.data.RoomRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class RoomController {
 
   private Logger logger = LoggerFactory.getLogger(RoomController.class);
 
-  private final HostRepository hostRepository;
-
-  public RoomController(HostRepository hostRepository) {
-    this.hostRepository = hostRepository;
-  }
-
-  @Autowired
-  private RoomRepository roomRepository;
-
-  @Autowired
-  private RoomService roomService;
+  private final RoomService roomService;
 
   @GetMapping("/")
-  public ResponseEntity viewRooms(@RequestParam(value = "checkin", required = false) String checkin,
+  public ResponseEntity<List<RoomResponseDto>> viewFilteredRooms(
+      @RequestParam(value = "checkin", required = false) String checkin,
       @RequestParam(value = "checkout", required = false) String checkout,
       @RequestParam(value = "adults", required = false) Integer adults,
       @RequestParam(value = "price_min", required = false) Integer priceMin,
       @RequestParam(value = "price_max", required = false) Integer priceMax,
       @RequestParam(value = "location", required = false) String location) {
 
-    if (location != null) {
-      return ResponseEntity.ok(roomService.findByLocation(location));
-    }
-    return ResponseEntity.ok(roomService.findAll());
+    return new ResponseEntity<>(roomService.findByLocation(location), HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/", params = {"!checkin", "!checkout", "!adults", "!price_min", "!price_max",
+      "!location"})
+  public ResponseEntity<List<RoomResponseDto>> viewAllRooms() {
+    return new ResponseEntity<>(roomService.findAll(), HttpStatus.OK);
   }
 }
