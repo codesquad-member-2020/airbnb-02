2cf5914575ddf19d2079f368e905ff1461044598
seizze <cwcwc3@gmail.com>
2020-05-26 00:09:58 +0900

    [#49] feat: 필터 헤더 뷰 커스텀 클래스 구현

---
+++ iOS/Airbnb/Airbnb/Utils/NibLoadable.swift
@@ -0,0 +1,19 @@
+//
+//  NibLoadable.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/05/25.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+protocol NibLoadable: IdentifiableView { }
+
+extension NibLoadable {
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: Self.identifier, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
---
+++ iOS/Airbnb/Airbnb/Views/FilterHeaderView.swift
@@ -0,0 +1,31 @@
+//
+//  FilterHeaderView.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/05/25.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class FilterHeaderView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+
+    private func configureView() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
+
+extension FilterHeaderView: NibLoadable { }
