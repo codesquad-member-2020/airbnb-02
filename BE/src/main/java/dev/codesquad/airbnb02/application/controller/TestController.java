61d250746bf1d39d42287f2e0f2f63d78ccfb945
wooody92 <pkower4@naver.com>
2020-05-25 17:52:08 +0900

    [#52] feat : 숙소 리스트 필터링을 위한 query parameter 작성
    - 날짜, 인원, 가격, 위치 정보를 갖는 query parameter 작성했습니다.

--- BE/src/main/java/dev/codesquad/airbnb02/application/controller/TestController.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/controller/TestController.java
@@ -6,25 +6,31 @@ import dev.codesquad.airbnb02.domain.room.data.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    private final HostRepository hostRepository;
+  private final HostRepository hostRepository;
 
-    public TestController(HostRepository hostRepository) {
-        this.hostRepository = hostRepository;
-    }
+  public TestController(HostRepository hostRepository) {
+    this.hostRepository = hostRepository;
+  }
 
-    @Autowired
-    private RoomRepository roomRepository;
+  @Autowired
+  private RoomRepository roomRepository;
 
-    @Autowired
-    private RoomService roomService;
+  @Autowired
+  private RoomService roomService;
 
-    @GetMapping("/")
-    public ResponseEntity test() {
-        return ResponseEntity.ok(roomService.findAll());
-    }
+  @GetMapping("/")
+  public ResponseEntity viewRooms(@RequestParam(value = "checkin", required = false) String checkin,
+      @RequestParam(value = "checkout", required = false) String checkout,
+      @RequestParam(value = "adults", required = false) Integer adults,
+      @RequestParam(value = "price_min", required = false) Integer priceMin,
+      @RequestParam(value = "price_max", required = false) Integer priceMax,
+      @RequestParam(value = "location", required = false) String location) {
+    return ResponseEntity.ok(roomService.findAll());
+  }
 }
