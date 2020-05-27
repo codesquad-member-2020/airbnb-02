ef1ed30b5bca942657a414bb07fa0dc6d3995923
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-27 13:26:46 +0900

    feat: Booking to have many-to-one relationship with room

--- BE/src/main/java/dev/codesquad/airbnb02/domain/room/entity/Booking.java
+++ BE/src/main/java/dev/codesquad/airbnb02/domain/room/entity/Booking.java
@@ -3,16 +3,20 @@ package dev.codesquad.airbnb02.domain.room.entity;
 import java.time.LocalDate;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Getter
+@Setter
 @ToString
 public class Booking {
 
@@ -21,9 +25,13 @@ public class Booking {
   private Long id;
 
   @ManyToOne
-  @JoinColumn(name = "room_id")
+  @JoinColumn(foreignKey = @ForeignKey(name = "room_id"))
+  @NotNull
   private Room room;
 
+  @NotNull
   private LocalDate bookDate;
+
+  @NotNull
   private int guest;
 }
