c09c10f52826fd1d7dfcbfef0bcff72593ccd5b9
wooody92 <pkower4@naver.com>
2020-05-29 01:33:02 +0900

    [#75] refactor : 숙소 예약 및 취소 요청 메서드 양식 변경
    - POST, DELETE MAPPING으로 요청양식 변경했습니다.

--- BE/src/main/java/dev/codesquad/airbnb02/application/controller/BookingController.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/controller/BookingController.java
@@ -7,8 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class BookingController {
 
   private final RoomService roomService;
 
-  @GetMapping("/create/{roomId}")
+  @PostMapping("/{roomId}")
   public ResponseEntity createBooking(
       @PathVariable Long roomId,
       @RequestParam(value = "checkin", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkin,
@@ -31,7 +32,7 @@ public class BookingController {
     return new ResponseEntity(HttpStatus.OK);
   }
 
-  @GetMapping("/remove/{roomId}")
+  @DeleteMapping("/{roomId}")
   public ResponseEntity removeBooking(
       @PathVariable Long roomId,
       @RequestParam(value = "checkin", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkin,
