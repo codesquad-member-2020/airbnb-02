c8e605deb3f45af1172fb4676de76ef723f8eb51
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-28 12:02:43 +0900

    feat: implement UserFavoriteResponseDto for receiving userId manually

--- BE/src/main/java/dev/codesquad/airbnb02/application/controller/UserController.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/controller/UserController.java
@@ -1,6 +1,6 @@
 package dev.codesquad.airbnb02.application.controller;
 
-import dev.codesquad.airbnb02.application.dto.FavoriteResponseDto;
+import dev.codesquad.airbnb02.application.dto.UserFavoriteResponseDto;
 import dev.codesquad.airbnb02.domain.user.business.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +18,13 @@ public class UserController {
   private final UserService userService;
 
   @PutMapping("/{userId}/favorite/add")
-  public ResponseEntity<FavoriteResponseDto> addFavorite(@PathVariable Long userId,
+  public ResponseEntity<UserFavoriteResponseDto> addFavorite(@PathVariable Long userId,
       @RequestParam Long roomid) {
     return ResponseEntity.ok(userService.addFavorite(userId, roomid));
   }
 
   @PutMapping("/{userId}/favorite/delete")
-  public ResponseEntity<FavoriteResponseDto> deleteFavorite(@PathVariable Long userId,
+  public ResponseEntity<UserFavoriteResponseDto> deleteFavorite(@PathVariable Long userId,
       @RequestParam Long roomid) {
     return ResponseEntity.ok(userService.deleteFavorite(userId, roomid));
   }
