e021c53056def8c4459cfd209f5f790ab89edced
ehgud0670 <kimdo2297@gmail.com>
2020-05-28 14:34:11 +0900

    [#44] feat: Alamofire의 Session이 NetworkDispatcher를 채택함

---
+++ iOS/Airbnb/Airbnb/Networking/NetworkDispatcher.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkDispatcher.swift
+//  Airbnb
+//
+//  Created by kimdo2297 on 2020/05/28.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+import Alamofire
+
+protocol NetworkDispatcher {
+    func excute(request: Request, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ())
+}
+
+extension NetworkDispatcher {}
+
+extension Session: NetworkDispatcher {
+    func excute(request: Request, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        guard let urlRequest = try? request.urlRequest() else { return }
+        self.request(urlRequest).validate().response { afDataResponse in
+            completionHandler(afDataResponse.data, afDataResponse.response, afDataResponse.error)
+        }
+    }
+}
