9ac11322b7f3b235737c34f2ceaed44a12ca6919
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-28 10:21:10 +0900

    feat: addFavorite/deleteFavorite feature in User entity

--- BE/src/main/java/dev/codesquad/airbnb02/domain/user/entity/User.java
+++ BE/src/main/java/dev/codesquad/airbnb02/domain/user/entity/User.java
@@ -2,6 +2,8 @@ package dev.codesquad.airbnb02.domain.user.entity;
 
 import dev.codesquad.airbnb02.domain.favorite.Favorite;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -11,9 +13,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class User {
 
   @Id
@@ -27,4 +31,31 @@ public class User {
   @CollectionTable(name = "favorite",
       joinColumns = @JoinColumn(name = "user_id", insertable = false, updatable = false))
   private List<Favorite> favorites;
+
+  public Favorite addFavorite(Long roomId) {
+    if (Objects.nonNull(findFavoriteByRoomId(roomId))) {
+      throw new RuntimeException("already exists.");
+    }
+    Favorite favorite = Favorite.builder()
+        .roomId(roomId)
+        .userId(this.id)
+        .favor(true)
+        .build();
+    this.favorites.add(favorite);
+    return favorite;
+  }
+
+  public Favorite deleteFavorite(Long roomId) {
+    Favorite favorite = Optional.ofNullable(findFavoriteByRoomId(roomId))
+        .orElseThrow(RuntimeException::new);
+    this.favorites.remove(favorite);
+    return favorite;
+  }
+
+  public Favorite findFavoriteByRoomId(Long roomId) {
+    return this.favorites.stream()
+        .filter(favorites -> favorites.getRoomId().equals(roomId))
+        .findAny()
+        .orElse(null);
+  }
 }
