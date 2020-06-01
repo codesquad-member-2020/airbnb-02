52b4a0ed6e6ab86fd1631b2c83d13bf88cad8e6e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-01 20:04:38 +0900

    refactor: change signature addLikedRoom to addBookmark

--- BE/src/main/java/dev/codesquad/airbnb02/domain/user/business/UserService.java
+++ BE/src/main/java/dev/codesquad/airbnb02/domain/user/business/UserService.java
@@ -1,7 +1,5 @@
 package dev.codesquad.airbnb02.domain.user.business;
 
-import java.util.Objects;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,31 +23,28 @@ public class UserService {
 	}
 
 	@Transactional
-	public RoomResponseDto addLikedRoom(Long userId, Long roomId) {
+	public RoomResponseDto addBookmark(Long userId, Long roomId) {
 		User user = userRepository.findById(userId).orElseThrow(NotFoundDataException::new);
 		Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
-		user.addLikedRoom(room);
+		user.addBookmark(room);
 		userRepository.save(user);
-		//TODO: 아래 하드코딩한 favorite 결과를 리팩토링 어떻게 할 지 고민해본다.
 		return RoomResponseDto.create(room, true);
 	}
 
 	@Transactional
-	public RoomResponseDto deleteLikedRoom(Long userId, Long roomId) {
+	public RoomResponseDto removeBookmark(Long userId, Long roomId) {
 		User user = userRepository.findById(userId).orElseThrow(NotFoundDataException::new);
 		Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
-		user.deleteLikedRoom(room);
+		user.removeBookmark(room);
 		userRepository.save(user);
-		//TODO: 아래 하드코딩한 favorite 결과를 리팩토링 어떻게 할 지 고민해본다.
 		return RoomResponseDto.create(room, false);
 	}
 
 	@Transactional(readOnly = true)
-	public boolean findLikedRoomByUserIdAndRoomId(Long userId, Long roomId) {
+	public boolean isUserBookmarkedRoom(Long userId, Long roomId) {
 		User user = userRepository.findById(userId).orElseThrow(NotFoundDataException::new);
-		// Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
-		//TODO: 아래 메소드가 roomId가 아니라 room을 받아서 처리하도록 리팩토링한다.
-		return ! Objects.isNull(user.findLikedRoomByRoomId(roomId));
+		Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
+		return user.isBookmarked(room);
 	}
 
 }
