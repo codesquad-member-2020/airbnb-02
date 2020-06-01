f7c17d5223d2f81fbeb45eedeb025ff63798c6c9
wooody92 <pkower4@naver.com>
2020-06-02 01:00:49 +0900

    [#116] feat : booking 예약, 취소 시 유저의 해당 숙소의 전체 예약정보를 보여주도록 DTO 구현
    - booking 예약, 취소 시 유저가 해당 숙소에 갖고있는 전체 예약정보를 보여주도록 DTO 구현

--- BE/src/main/java/dev/codesquad/airbnb02/application/dto/BookingResponseDto.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/dto/BookingResponseDto.java
@@ -2,7 +2,6 @@ package dev.codesquad.airbnb02.application.dto;
 
 import dev.codesquad.airbnb02.domain.room.entity.Booking;
 import dev.codesquad.airbnb02.domain.room.entity.Room;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -12,13 +11,17 @@ import lombok.ToString;
 @ToString
 public class BookingResponseDto {
 
+  private Long userId;
+  private Long roomId;
   private List<Booking> bookings = new ArrayList<>();
 
-  protected BookingResponseDto(Room room) {
-    this.bookings = room.getBookings();
+  private BookingResponseDto(Long userId, Room room) {
+    this.userId = userId;
+    this.roomId = room.getId();
+    this.bookings = room.findBookingsByUserId(userId);
   }
 
-  public static BookingResponseDto create(Room room) {
-    return new BookingResponseDto(room);
+  public static BookingResponseDto create(Long userId, Room room) {
+    return new BookingResponseDto(userId, room);
   }
 }
