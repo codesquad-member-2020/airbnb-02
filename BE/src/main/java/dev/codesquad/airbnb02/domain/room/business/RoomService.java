8172354b2293de4b4c35bc948b7b02b23576c43a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-01 18:53:48 +0900

    feat: RoomService to return RoomDetailResponseDto

--- BE/src/main/java/dev/codesquad/airbnb02/domain/room/business/RoomService.java
+++ BE/src/main/java/dev/codesquad/airbnb02/domain/room/business/RoomService.java
@@ -1,17 +1,19 @@
 package dev.codesquad.airbnb02.domain.room.business;
 
-import dev.codesquad.airbnb02.application.dto.BookingResponseDto;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dev.codesquad.airbnb02.application.dto.RoomDetailResponseDto;
 import dev.codesquad.airbnb02.application.dto.RoomResponseDto;
 import dev.codesquad.airbnb02.common.exception.BookingNotAllowedException;
 import dev.codesquad.airbnb02.common.exception.NotFoundDataException;
 import dev.codesquad.airbnb02.domain.room.data.RoomRepository;
 import dev.codesquad.airbnb02.domain.room.entity.Room;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -40,10 +42,15 @@ public class RoomService {
         .collect(Collectors.toList());
   }
 
+  public RoomDetailResponseDto findDetailByRoomId(Long roomId) {
+    Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
+    return RoomDetailResponseDto.create(room);
+  }
+
   @Transactional
   public void createBooking(Long roomId, LocalDate checkin, LocalDate checkout) {
     Room room = findRoom(roomId);
-    if (!room.isValidDate(checkin, checkout)) {
+    if (! room.isValidDate(checkin, checkout)) {
       throw new BookingNotAllowedException("이미 예약된 날짜가 존재 합니다.");
     }
     room.addBookings(checkin, checkout);
