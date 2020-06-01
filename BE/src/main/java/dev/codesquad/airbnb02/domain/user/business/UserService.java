c7add31ed42f058b6bcf4f6dddd879abf8ed7ddf
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-01 19:18:14 +0900

    feat: add logic for UserService to return user like room or not

--- BE/src/main/java/dev/codesquad/airbnb02/domain/user/business/UserService.java
+++ BE/src/main/java/dev/codesquad/airbnb02/domain/user/business/UserService.java
@@ -1,41 +1,52 @@
 package dev.codesquad.airbnb02.domain.user.business;
 
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import dev.codesquad.airbnb02.application.dto.RoomResponseDto;
+import dev.codesquad.airbnb02.common.exception.NotFoundDataException;
 import dev.codesquad.airbnb02.domain.room.data.RoomRepository;
 import dev.codesquad.airbnb02.domain.room.entity.Room;
 import dev.codesquad.airbnb02.domain.user.data.UserRepository;
 import dev.codesquad.airbnb02.domain.user.entity.User;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final RoomRepository roomRepository;
+	private final UserRepository userRepository;
+	private final RoomRepository roomRepository;
 
-  public UserService(UserRepository userRepository,
-      RoomRepository roomRepository) {
-    this.userRepository = userRepository;
-    this.roomRepository = roomRepository;
-  }
+	public UserService(UserRepository userRepository,
+		RoomRepository roomRepository) {
+		this.userRepository = userRepository;
+		this.roomRepository = roomRepository;
+	}
 
-  @Transactional
-  public RoomResponseDto addLikedRoom(Long userId, Long roomId) {
-    User user = userRepository.findById(userId).orElseThrow(RuntimeException::new);
-    Room room = roomRepository.findById(roomId).orElseThrow(RuntimeException::new);
-    user.addLikedRoom(room);
-    userRepository.save(user);
-    return RoomResponseDto.create(room);
-  }
+	@Transactional
+	public RoomResponseDto addLikedRoom(Long userId, Long roomId) {
+		User user = userRepository.findById(userId).orElseThrow(NotFoundDataException::new);
+		Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
+		user.addLikedRoom(room);
+		userRepository.save(user);
+		return RoomResponseDto.create(room);
+	}
 
-  @Transactional
-  public RoomResponseDto deleteLikedRoom(Long userId, Long roomId) {
-    User user = userRepository.findById(userId).orElseThrow(RuntimeException::new);
-    Room room = roomRepository.findById(roomId).orElseThrow(RuntimeException::new);
-    user.deleteLikedRoom(room);
-    userRepository.save(user);
-    return RoomResponseDto.create(room);
-  }
+	@Transactional
+	public RoomResponseDto deleteLikedRoom(Long userId, Long roomId) {
+		User user = userRepository.findById(userId).orElseThrow(NotFoundDataException::new);
+		Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
+		user.deleteLikedRoom(room);
+		userRepository.save(user);
+		return RoomResponseDto.create(room);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean findLikedRoomByUserIdAndRoomId(Long userId, Long roomId) {
+		User user = userRepository.findById(userId).orElseThrow(NotFoundDataException::new);
+		// Room room = roomRepository.findById(roomId).orElseThrow(NotFoundDataException::new);
+		return ! Objects.isNull(user.findLikedRoomByRoomId(roomId));
+	}
 
 }
