fee77c771485eadf42756c60bf8daf5dd7325cc5
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-01 20:07:09 +0900

    refactor: rename signature of method addLikedRoom with addBookmark

--- BE/src/main/java/dev/codesquad/airbnb02/application/controller/UserController.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/controller/UserController.java
@@ -1,14 +1,15 @@
 package dev.codesquad.airbnb02.application.controller;
 
-import dev.codesquad.airbnb02.application.dto.RoomResponseDto;
-import dev.codesquad.airbnb02.domain.user.business.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.codesquad.airbnb02.application.dto.RoomResponseDto;
+import dev.codesquad.airbnb02.domain.user.business.UserService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +21,12 @@ public class UserController {
   @GetMapping("/{userId}/favorite/add")
   public ResponseEntity<RoomResponseDto> addFavorite(@PathVariable Long userId,
       @RequestParam(value = "room_id") Long roomId) {
-    return ResponseEntity.ok(userService.addLikedRoom(userId, roomId));
+    return ResponseEntity.ok(userService.addBookmark(userId, roomId));
   }
 
   @GetMapping("/{userId}/favorite/delete")
   public ResponseEntity<RoomResponseDto> deleteFavorite(@PathVariable Long userId,
       @RequestParam(value = "room_id") Long roomId) {
-    return ResponseEntity.ok(userService.deleteLikedRoom(userId, roomId));
+    return ResponseEntity.ok(userService.removeBookmark(userId, roomId));
   }
 }
