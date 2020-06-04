0d7d9ebfc70d8a3b1f209228199722cbc6ef973b
seizze <cwcwc3@gmail.com>
2020-06-05 03:54:34 +0900

    [#142] feat: 캘린더 헤더 뷰 클래스 추가

---
+++ iOS/Airbnb/Airbnb/Views/CalendarHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarHeaderView.swift
+//  Airbnb
+//
+//  Created by Chaewan Park on 2020/06/05.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class CalendarHeaderView: UICollectionReusableView {
+    @IBOutlet weak var headerLabel: UILabel!
+}
+
+extension CalendarHeaderView: Identifiable { }
