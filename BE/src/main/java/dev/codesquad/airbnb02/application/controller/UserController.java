4975c46ee12cf11187ce0b0b92e5a9324f98daca
wooody92 <pkower4@naver.com>
2020-06-04 16:52:34 +0900

    [#135] refactor : 즐겨찾기 추가, 제거 시 userId를 parameter가 아니고 header token에서 정보를 가져오도록 변경

--- BE/src/main/java/dev/codesquad/airbnb02/application/controller/UserController.java
+++ BE/src/main/java/dev/codesquad/airbnb02/application/controller/UserController.java
@@ -1,9 +1,9 @@
 package dev.codesquad.airbnb02.application.controller;
 
-import dev.codesquad.airbnb02.common.jwt.JwtService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +18,16 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
   private final UserService userService;
-  private final JwtService jwtService;
 
-  @GetMapping("/{userId}/favorite/add")
-  public ResponseEntity<RoomResponseDto> addFavorite(@PathVariable Long userId,
-      @RequestParam(value = "room_id") Long roomId) {
+  @PostMapping("/favorite/add")
+  public ResponseEntity<RoomResponseDto> addFavorite(@RequestParam(value = "room_id") Long roomId) {
+    Long userId = userService.findUserIdByToken();
     return ResponseEntity.ok(userService.addBookmark(userId, roomId));
   }
 
-  @GetMapping("/{userId}/favorite/delete")
-  public ResponseEntity<RoomResponseDto> deleteFavorite(@PathVariable Long userId,
-      @RequestParam(value = "room_id") Long roomId) {
+  @DeleteMapping("/favorite/delete")
+  public ResponseEntity<RoomResponseDto> deleteFavorite(@RequestParam(value = "room_id") Long roomId) {
+    Long userId = userService.findUserIdByToken();
     return ResponseEntity.ok(userService.removeBookmark(userId, roomId));
   }
 
