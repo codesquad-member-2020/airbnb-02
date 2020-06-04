3e3665322cecfa94cb3c5f031791dac4e70572e3
seizze <cwcwc3@gmail.com>
2020-06-05 05:51:33 +0900

    [#142] test: month offset 메서드 테스트

--- iOS/Airbnb/AirbnbTests/CalendarViewModelTests.swift
+++ iOS/Airbnb/AirbnbTests/CalendarViewModelTests.swift
@@ -11,9 +11,29 @@ import XCTest
 
 class CalendarViewModelTests: XCTestCase {
     func testNumberOfMonths_ForTwoYears() {
-        let startDate = Date()
-        let endDate = Calendar.current.date(byAdding: DateComponents(year: 2), to: startDate)!
-        let viewModel = CalendarViewModel(startDate: startDate, endDate: endDate)
+        let viewModel = CalendarViewModel(
+            startDate: DateStubs.startDate,
+            endDate: DateStubs.endDateAfterTwoYears)
         XCTAssertEqual(viewModel.numberOfMonths(), 25)
     }
+    
+    func testYearAndMonth_After3MonthOffset() {
+        let viewModel = CalendarViewModel(
+            startDate: DateStubs.startDate,
+            endDate: DateStubs.endDateAfterOneYear)
+        let date = viewModel.date(withMonthOffsetFromToday: 3)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: date)
+        let expectedDateComponents = DateComponents(year: 2020, month: 9, day: 5)
+        XCTAssertEqual(dateComponents, expectedDateComponents)
+    }
+}
+
+struct DateStubs {
+    static let calendar = Calendar.current
+    static let oneYear = DateComponents(year: 1)
+    static let twoYears = DateComponents(year: 2)
+    
+    static let startDate = DateComponents(calendar: .current, year: 2020, month: 6, day: 5).date!
+    static let endDateAfterOneYear = calendar.date(byAdding: oneYear, to: startDate)!
+    static let endDateAfterTwoYears = calendar.date(byAdding: twoYears, to: startDate)!
 }
