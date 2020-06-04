669e7b616dd03c5000a536aad6faf9b522dc1118
seizze <cwcwc3@gmail.com>
2020-06-05 04:50:36 +0900

    [#142] feat: 페이징 자연스럽도록 개선

--- iOS/Airbnb/Airbnb/Delegates/CalendarLayout.swift
+++ iOS/Airbnb/Airbnb/Delegates/CalendarLayout.swift
@@ -11,6 +11,7 @@ import UIKit
 final class CalendarLayout: NSObject {
     private var sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 40, right: 0)
     private var cellSize: CGSize?
+    private var startOffset: CGFloat?
 }
 
 extension CalendarLayout: UICollectionViewDelegateFlowLayout {
@@ -33,6 +34,10 @@ extension CalendarLayout: UICollectionViewDelegateFlowLayout {
 }
 
 extension CalendarLayout: UIScrollViewDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        startOffset = scrollView.contentOffset.y
+    }
+    
     func scrollViewWillEndDragging(
         _ scrollView: UIScrollView,
         withVelocity velocity: CGPoint,
@@ -40,15 +45,15 @@ extension CalendarLayout: UIScrollViewDelegate {
 
         guard let view = scrollView as? UICollectionView,
             let layout = view.collectionViewLayout as? UICollectionViewFlowLayout,
-            let cellHeight = cellSize?.height else { return }
+            let cellHeight = cellSize?.height,
+            let startOffset = startOffset else { return }
         
-        let targetOffset = targetContentOffset.pointee.y
         let totalHeight = layout.headerReferenceSize.height + cellHeight * 6 + sectionInset.bottom
         
         if scrollView.contentOffset.y > targetContentOffset.pointee.y {
-            targetContentOffset.pointee.y = totalHeight * floor(targetOffset / totalHeight)
+            targetContentOffset.pointee.y = startOffset - totalHeight
         } else {
-            targetContentOffset.pointee.y = totalHeight * ceil(targetOffset / totalHeight)
+            targetContentOffset.pointee.y = startOffset + totalHeight
         }
     }
 }
