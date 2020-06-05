06b0ff692404da9238a17abac72d66d9fe533f7e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-05 16:15:19 +0900

    test: test for return nearest room list

--- BE/src/test/java/dev/codesquad/airbnb02/common/GeometryTest.java
+++ BE/src/test/java/dev/codesquad/airbnb02/common/GeometryTest.java
@@ -1,14 +1,27 @@
 package dev.codesquad.airbnb02.common;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.io.ParseException;
-import org.locationtech.jts.io.WKTReader;
+import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+import dev.codesquad.airbnb02.application.dto.RoomResponseDtoSimplified;
+import dev.codesquad.airbnb02.domain.room.data.RoomDao;
+
+@SpringBootTest
 public class GeometryTest {
+
+	@Autowired
+	private RoomDao roomDao;
 
 	@Test
 	public void shouldConvertWktToGeometry() throws ParseException {
@@ -20,5 +33,16 @@ public class GeometryTest {
 
 	public Geometry wktToGeometry(String wellKnownText) throws ParseException {
 		return new WKTReader().read(wellKnownText);
+	}
+
+	@DisplayName("주어진 좌표에 따라 주위 5km 근처의 방이 가까운 순서대로 주어진다.")
+	@Test
+	void 현재_위치_기준으로_가까운_방의_리스트가_리턴된다() {
+		double lon = 127.06533399999999;
+		double lat = 37.493712000000002;
+		int length = 5000;
+		assertThat(roomDao.findNearestRoom(lon, lat, length)).isNotNull();
+		assertThat(roomDao.findNearestRoom(lon, lat, length)).isInstanceOf(List.class);
+		assertThat(roomDao.findNearestRoom(lon, lat, length).get(0)).isInstanceOf(RoomResponseDtoSimplified.class);
 	}
 }
