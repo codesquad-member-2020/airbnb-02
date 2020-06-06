85ed278b24e031fac3ef643997863107d1cca073
seizze <cwcwc3@gmail.com>
2020-06-07 00:51:01 +0900

    [#153] feat: 셀 상태별 외형 구현

--- iOS/Airbnb/Airbnb/Views/CalendarCell.swift
+++ iOS/Airbnb/Airbnb/Views/CalendarCell.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class CalendarCell: UICollectionViewCell {
+    enum State {
+        case startSelected, endSelected, staying, normal
+    }
+    
     @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var leftGrayView: UIView!
+    @IBOutlet weak var rightGrayView: UIView!
+    @IBOutlet weak var blackRoundView: UIView!
+    
+    var state: State = .normal {
+        didSet { changeState(to: state) }
+    }
     
     override func prepareForReuse() {
         dayLabel.text = nil
+        setAllViewsHidden()
+    }
+    
+    private func changeState(to state: State) {
+        setAllViewsHidden()
+        switch state {
+        case .startSelected:
+            blackRoundView.isHidden = false
+            rightGrayView.isHidden = false
+        case .endSelected:
+            blackRoundView.isHidden = false
+            leftGrayView.isHidden = false
+        case .staying:
+            rightGrayView.isHidden = false
+            leftGrayView.isHidden = false
+        case .normal: break
+        }
+    }
+    
+    private func setAllViewsHidden() {
+        leftGrayView.isHidden = true
+        rightGrayView.isHidden = true
+        blackRoundView.isHidden = true
     }
 }
 
