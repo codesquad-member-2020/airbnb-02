c468e7fc2cc778df99e43f52866c8ca23d18534d
ehgud0670 <kimdo2297@gmail.com>
2020-06-30 20:16:23 +0900

    [#137] refactor: 약어 대신 cgContext라고 이름 수정

--- iOS/Airbnb/Airbnb/Views/RangeSliderTrackLayer.swift
+++ iOS/Airbnb/Airbnb/Views/RangeSliderTrackLayer.swift
@@ -11,23 +11,23 @@ import UIKit
 final class RangeSliderTrackLayer: CALayer {
     weak var rangeSlider: RangeSlider?
     
-    override func draw(in ctx: CGContext) {
+    override func draw(in cgContext: CGContext) {
         guard let slider = rangeSlider else {
           return
         }
         
         let path = UIBezierPath(roundedRect: slider.bounds, cornerRadius: cornerRadius)
-        ctx.addPath(path.cgPath)
+        cgContext.addPath(path.cgPath)
         
-        ctx.setFillColor(slider.trackTintColor.cgColor)
-        ctx.fillPath()
+        cgContext.setFillColor(slider.trackTintColor.cgColor)
+        cgContext.fillPath()
         
-        ctx.setFillColor(slider.trackHighlightTintColor.cgColor)
+        cgContext.setFillColor(slider.trackHighlightTintColor.cgColor)
         let lowerValuePosition = slider.positionForValue(slider.lowerValue)
         let upperValuePosition = slider.positionForValue(slider.upperValue)
         let rect = CGRect(x: lowerValuePosition, y: 0,
                           width: upperValuePosition - lowerValuePosition,
                           height: bounds.height)
-        ctx.fill(rect)
+        cgContext.fill(rect)
     }
 }
