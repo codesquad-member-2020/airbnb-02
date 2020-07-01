5b014651c50ce5f6a6d615640e4da024c7bf55be
ehgud0670 <kimdo2297@gmail.com>
2020-07-01 17:57:33 +0900

    [#144] fix: index out of range 런타임 에러 안나게 코드 수정 및 중복 부분 제거

--- iOS/Airbnb/Airbnb/ViewModels/PriceViewModel.swift
+++ iOS/Airbnb/Airbnb/ViewModels/PriceViewModel.swift
@@ -25,19 +25,19 @@ final class PriceViewModel {
     }
     
     func priceRangeText(minimumPercent: CGFloat = 0, maximumPercent: CGFloat = 1) -> String? {
-        guard let minPrice = formatter.string(from:
-            prices[firstIndex(minimumPercent: minimumPercent)].key as NSNumber),
-            let maxPrice = formatter.string(from:
-                prices[lastIndex(maximumPercent: maximumPercent)].key as NSNumber)
+        let firstIndex = index(percent: minimumPercent)
+        let lastIndex = index(percent: maximumPercent)
+        guard let minPrice = formatter.string(from: prices[firstIndex].key as NSNumber),
+            let maxPrice = formatter.string(from: prices[lastIndex].key as NSNumber)
             else { return nil }
         
         return "\(minPrice)원부터 \(maxPrice)원 이상"
     }
     
     func priceAvarageText(minimumPercent: CGFloat = 0, maximumPercent: CGFloat = 1) -> String? {
-        guard let avarage = generateAverage(minimumPercent: minimumPercent,
-                                            maximumPercent: maximumPercent),
-            let avaragePrice = formatter.string(from: avarage as NSNumber) else { return nil }
+        guard let avarage = generateAverage(minimumPercent: minimumPercent, maximumPercent: maximumPercent), let avaragePrice = formatter.string(
+            from: avarage as NSNumber
+            ) else { return nil }
         
         return  "일박 평균 가격은 \(avaragePrice)원"
     }
@@ -46,7 +46,10 @@ final class PriceViewModel {
         var totalPrice = 0
         var totalCount = 0
         
-        for index in firstIndex(minimumPercent: minimumPercent) ... lastIndex(maximumPercent: maximumPercent) {
+        let firstIndex = index(percent: minimumPercent)
+        let lastIndex = index(percent: maximumPercent)
+        guard firstIndex <= lastIndex else { return nil }
+        for index in firstIndex ... lastIndex {
             totalPrice += prices[index].key * prices[index].value
             totalCount += prices[index].value
         }
@@ -55,13 +58,9 @@ final class PriceViewModel {
         return Int(totalPrice / totalCount)
     }
     
-    private func firstIndex(minimumPercent: CGFloat) -> Int {
-        guard Int(minimumPercent * CGFloat(prices.count)) > 0 else { return 0 }
-        return Int(minimumPercent * CGFloat(prices.count))
-    }
-    
-    private func lastIndex(maximumPercent: CGFloat) -> Int {
-        guard Int(maximumPercent * CGFloat(prices.count)) < prices.count else { return prices.count - 1 }
-        return Int(maximumPercent * CGFloat(prices.count))
+    private func index(percent: CGFloat) -> Int {
+        guard Int(percent * CGFloat(prices.count)) > 0 else { return 0 }
+        guard Int(percent * CGFloat(prices.count)) < prices.count else { return prices.count - 1 }
+        return Int(percent * CGFloat(prices.count))
     }
 }
